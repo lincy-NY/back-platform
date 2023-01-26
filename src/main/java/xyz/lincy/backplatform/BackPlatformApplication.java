f4595ac0528559aaff27db5c642136995fecb496
linchangyu <linchangyu@apexsoft.com.cn>
2023-01-26 22:13:45 +0800

    first commit

---
+++ src/main/java/xyz/lincy/backplatform/BackPlatformApplication.java
@@ -0,0 +1,13 @@
+package xyz.lincy.backplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/lincy/backplatform/BackPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lincy.backplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
